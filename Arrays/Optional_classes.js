428938a74a4a85d3c6f5b2c8bb0cf08701207891
anujtodankar <anujtodankar020@gmail.com>
2022-06-29 00:39:08 +0530

    Array - Optional Classes(Reference Type)

---
+++ Arrays/Optional_classes.js
@@ -0,0 +1,9 @@
+
+//reference Type
+const Object1 ={value:10}
+const Object2 = Object1
+
+const Object3 = {value:10}
+console.log("//True Value",Object1 == Object2);
+console.log("//False Value",Object1 == Object3);
+
